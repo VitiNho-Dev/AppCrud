0ba305306fd178b13273b23c6b2b2b37a0b5e2eb
Victor Oliveira <victorhenrique121413@gmail.com>
2024-03-29 10:38:02 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AppCrud.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ ViewsModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using AppCrud.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Text.Json;
+
+
+
+using System.Windows.Input;
+
+namespace AppCrud.ViewsModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        HttpClient httpClient;
+        JsonSerializerOptions _serializerOptions;
+        string baseUrl = "http://localhost:5282";
+
+        [ObservableProperty]
+        public int _productId;
+        [ObservableProperty]
+        public string _productName;
+        [ObservableProperty]
+        public double _productPrice;
+        [ObservableProperty]
+        public Product _product;
+        [ObservableProperty]
+        public ObservableCollection<Product> _products;
+
+        public MainViewModel()
+        {
+            httpClient = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            Products = new ObservableCollection<Product>();
+        }
+
+        public ICommand GetAllProduct => new Command(async () => await LoadProductAsync());
+
+        private async Task LoadProductAsync()
+        {
+            var url = $"{baseUrl}/ProductController";
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using (var responseStream = await response.Content.ReadAsStreamAsync())
+                {
+                    var data = await JsonSerializer.DeserializeAsync<ObservableCollection<Product>>(responseStream);
+                    Products = data;
+                }
+            }
+        }
+    }
+}
